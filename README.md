a6d91e5558431a3e4a33fb3df1c601b90de76fdf
Rupert Wu <wu.rupert@outlook.com>
2024-09-23 21:44:14 -0400

    set up as python package, updated README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Neural Collapse
+
+## Installation
+
+```sh
+# install from remote
+pip install git+https://github.com/username/repository.git
+
+# install locally
+pip install -e .
+```
+
+## Import
+
+```py
+import neural_collapse as nc
+```
+
+## References
+
+- [Neural Collapse: A Review on Modelling Principles and Generalization](https://arxiv.org/abs/2206.04041)
+- [Prevalence of neural collapse during the terminal phase of deep learning training](https://www.pnas.org/doi/full/10.1073/pnas.2015509117)
---
+++ setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import find_packages, setup
+
+setup(
+    name="neural_collapse",
+    version="0.1",
+    author="Robert Wu",
+    author_email="rupert@cs.toronto.edu",
+    description="A generic library for accumulating feature statistics and computing neural collapse metrics.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rhubarbwu/neural-collapse",  # Link to your repository
+    packages=find_packages(),  # Automatically find the 'matrix_operator' package
+    install_requires=[
+        "faiss-gpu",
+        "numpy<2",
+        "torch",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
